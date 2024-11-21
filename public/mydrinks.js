493bfa75b14a69aefc12589315bfa391f3b1f1d7
morten <Morten.toft.hansen2@hotmail.dk>
2024-11-21 11:36:43 +0100

    mydrinks output desgin

--- public/mydrinks.js
+++ public/mydrinks.js
@@ -2,51 +2,51 @@ import { sendHttpGetRequest } from "./common.js";
 
 
 const searchdrinks = document.getElementById("drinksSearch");
-searchdrinks.addEventListener('keypress', ()=>{
+searchdrinks.addEventListener('keypress', () => {
     const value2 = searchdrinks.value
     console.log(value2)
 })
 const checkboxTable = [
-    { id: "checkbox-gin",                   value: "gin" },
-    { id: "checkbox-vodka",                 value: "vodka" },
-    { id: "checkbox-rom",                   value: "rom" },
-    { id: "checkbox-mango-sirup",           value: "mango-sirup" },
-    { id: "checkbox-blue-curacao",          value: "blue-curacao" },
-    { id: "checkbox-passionfruit-sirup",    value: "passionfruit-sirup" },
-    { id: "checkbox-grenadine",             value: "grenadine" },
-    { id: "checkbox-fanta",                 value: "fanta" },
-    { id: "checkbox-cola",                  value: "cola" },
-    { id: "checkbox-schweppes",             value: "schweppes" },
-    { id: "checkbox-faxe-kondi",            value: "faxe-kondi" },
-    { id: "checkbox-booster",               value: "booster" },
-    { id: "checkbox-redbull",               value: "redbull" },
-    { id: "checkbox-æblejuice",             value: "æblejuice" },
+    { id: "checkbox-gin", value: "gin" },
+    { id: "checkbox-vodka", value: "vodka" },
+    { id: "checkbox-rom", value: "rom" },
+    { id: "checkbox-mango-sirup", value: "mango-sirup" },
+    { id: "checkbox-blue-curacao", value: "blue-curacao" },
+    { id: "checkbox-passionfruit-sirup", value: "passionfruit-sirup" },
+    { id: "checkbox-grenadine", value: "grenadine" },
+    { id: "checkbox-fanta", value: "fanta" },
+    { id: "checkbox-cola", value: "cola" },
+    { id: "checkbox-schweppes", value: "schweppes" },
+    { id: "checkbox-faxe-kondi", value: "faxe-kondi" },
+    { id: "checkbox-booster", value: "booster" },
+    { id: "checkbox-redbull", value: "redbull" },
+    { id: "checkbox-æblejuice", value: "æblejuice" },
 ]
 
 
 
 
-function getActivatedFilters (){
+function getActivatedFilters() {
     let filters = []
-    for (const {id, value} of checkboxTable){
+    for (const { id, value } of checkboxTable) {
         const ischecked = document.getElementById(id).checked;
-        if (ischecked){
+        if (ischecked) {
             filters.push(value)
         }
     }
     return filters
 }
 
-async function performFilterQuery (){
+async function performFilterQuery() {
     //Clear search bar
     searchdrinks.value = ""
     //get filters
     const filters = getActivatedFilters()
     //Send filter req
     const responseData = await sendHttpGetRequest('/mydrinks_filter', {
-        filter: filters, 
+        filter: filters,
 
-    })  
+    })
 
     //show results
     console.log(responseData)
@@ -59,43 +59,48 @@ async function performFilterQuery (){
 
 
 
-for (const {id, value} of checkboxTable){
-    document.getElementById(id).addEventListener("change", ()=>{
+for (const { id, value } of checkboxTable) {
+    document.getElementById(id).addEventListener("change", () => {
         console.log(getActivatedFilters())
-        
+
     });
 }
 
 
 const filterslist = document.getElementById("apply-filters");
 filterslist.addEventListener('click', async () => {
-     console.log(getActivatedFilters())
-     performFilterQuery()
+    console.log(getActivatedFilters())
+    performFilterQuery()
+    drinksoutput()
 });
 
 // inside html 
 //  <div id="my-div"></div>
 
-// const myDiv = document.getElementById("my-div");
-
-// const myPlayers = [
-//     { lvl: 44, name: "jeppe",  id: 4 },
-//     { lvl: 34, name: "jeppe",  id: 22 },
-//     { lvl: 9,  name: "morben", id: 213123 },
-// ]
-
-// myDiv.innerHTML = myPlayers
-//     .map((player) => {
-//         return `
-//             <div class="player" id="player-${player.id}">
-//                 <p1>name is ${player.name} and lvl is ${player.lvl}</p1>
-//             </div>
-//         `;
-//     })
-//     .join("");
+async function drinksoutput() {
 
 
-// inside html 
+    const myoutput = document.getElementById("my-drinks");
+
+    const Outputdata = [
+        { lvl: 44, name: "jeppe", id: 4 },
+        { lvl: 34, name: "jeppe", id: 22 },
+        { lvl: 9, name: "morben", id: 213123 },
+    ]
+
+    myoutput.innerHTML = Outputdata
+        .map((Outputdata) => {
+            return `
+                 <div class="drinksoutput">
+                     <p1>name is ${Outputdata.name} and lvl is ${Outputdata.lvl}</p1>
+                 </div>
+             `;
+        })
+        .join("");
+}
+
+
+// inside html
 //  <div id="my-div">
 //      <div class="player" id="player-4">
 //          <p1>name is jeppe and lvl is 44</p1>
