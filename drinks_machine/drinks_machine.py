5132e003eb1ab26e90be785614e638139f7929cd
Jeppe <jemo@mercantec.dk>
2024-12-10 14:54:23 +0100

    correction of direction

--- drinks_machine/drinks_machine.py
+++ drinks_machine/drinks_machine.py
@@ -4,6 +4,16 @@ from pump import Pump
 from rotary_encoder import RotaryEncoder
 from time import sleep 
 
+class DrinksIdSelector:
+    def __init__(self) -> None:
+        self.current_id = 0
+
+    def next_drink(self) -> None:
+        self.current_id += 1
+
+    def prev_drink(self) -> None:
+        self.current_id += 1
+
 class DrinksMachine:
     def __init__(self):
         self.database = Database()
@@ -13,26 +23,20 @@ class DrinksMachine:
         self.display = Display()
         self.rotary_encoder = RotaryEncoder()
 
-        # self.last_re_counter = self.rotary_encoder.counter()
-    
+        self.drinks_id_sel = DrinksIdSelector()
+
     def update(self) -> None:
         self.rotary_encoder.update()
 
         if self.rotary_encoder.has_rotated_clockwise():
+            self.drinks_id_sel.next_drink()
             print("rotated clockwise")
             self.rotary_encoder.reset()
             
         if self.rotary_encoder.has_rotated_counter_clockwise():
+            self.drinks_id_sel.prev_drink()
             print("rotated counter clockwise")
             self.rotary_encoder.reset()
-        
-        # new_re_counter = self.rotary_encoder.counter()
-        # if abs(self.last_re_counter - new_re_counter) == 1:
-        #     print(f"last: {self.last_re_counter}")
-        #     print(f"new: {new_re_counter}")
-        #     sleep(0.05)
-        #     self.pumps[0].start(0.5)
-        #     self.last_re_counter = new_re_counter
 
     def destroy(self) -> None:
         self.destroy_pumps()
